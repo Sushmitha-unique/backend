17e588aac33b84109d27b2fe659114e78379100a
B.sushmitha <106570754+Sushmitha-unique@users.noreply.github.com>
2024-06-10 10:38:54 +0530

    Update login.js

--- routes/login.js
+++ routes/login.js
@@ -14,7 +14,7 @@ router.post("/",async (req,res)=>{
                     }
                 }
                   
-           const token = jwt.sign(data,'secret_ecom')
+           const token = jwt.sign(data,'secret_ecom',{ expiresIn: '2h' })
             res.json({success:true,token})
             }
             else{
